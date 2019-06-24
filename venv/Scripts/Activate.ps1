002bcc41fa795b9e960481160e13faed41bba17b
Mika Lahtinen <image1224@outlook.com>
2019-06-24 22:04:23 +0800

    fix some mistakes

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="F:\My Tasks\Blog-by-Flask\venv"
+$env:VIRTUAL_ENV="F:\My Tasks\Blog-by-Flask\Blog-by-Flask\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=F:\My Tasks\Blog-by-Flask\venv"
+set "VIRTUAL_ENV=F:\My Tasks\Blog-by-Flask\Blog-by-Flask\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
