4714cb6de8f33fb5377cea31f02d5f4318b98ab2
Mika Lahtinen <image1224@outlook.com>
2019-06-24 23:43:49 +0800

    add password plugins

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -21,3 +21,19 @@ class User(db.Model):
         return '<User %r>' % self.table_username
 
     role_id = db.Column(db.Integer, db.ForeignKey('roles.table_id'))
+
+    '''
+    Add password hashes
+    '''
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
